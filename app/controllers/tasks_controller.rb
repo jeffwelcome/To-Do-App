b32d31b2fef0b30e2ff628913de7295036229a4c
Jeff Lloyd <jeffwelcome@icloud.com>
2015-02-01 13:32:43 +0000

    Refactored our tables and added state condition

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,44 +1,52 @@
 class TasksController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :set_task, only: [:show, :edit, :update, :destroy, :change]
 
-respond_to :html
+  respond_to :html
 
-def index
-@to_do = current_user.tasks.where(state: "to_do")
-@doing = current_user.tasks.where(state: "doing")
-@done = current_user.tasks.where(state: "done")
-respond_with(@task)
-end 
+  def index
+    @to_do = current_user.tasks.where(state: "to_do")
+    @doing = current_user.tasks.where(state: "doing")
+    @done = current_user.tasks.where(state: "done")
+    respond_with(@tasks)
+  end
 
-def show
-  respond_with(@task)
-end
+  def show
+    respond_with(@task)
+  end
 
-def new
-  @task = Task.new
-  respond_with(@task)
-end
+  def new
+    @task = Task.new
+    respond_with(@task)
+  end
 
   def edit
   end
 
-def create
-@task = current_user.tasks.new(task_params)
-@task.save
-respond_with(@task)
-end
+  def create
+    @task = current_user.tasks.new(task_params)
+    @task.save
+    respond_with(@task)
+  end
 
   def update
     @task.update(task_params)
     respond_with(@task)
-      end
+  end
 
   def destroy
     @task.destroy
     respond_with(@task)
-    end
+  end
 
+  def change
+    @task.update_attributes(state: params[:state])
+    respond_to do |format|
+      format.html {redirect_to tasks_path, notice: "Task Update"}
+    end
+  end
+
+  private
     def set_task
       @task = Task.find(params[:id])
     end
@@ -46,4 +54,4 @@ end
     def task_params
       params.require(:task).permit(:content, :state)
     end
-end    
+end
